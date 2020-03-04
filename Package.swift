df2f5d92221ec03c06dc7d92feff878cac4328a0
snaill <snaill@jeebook.com>
2020-03-04 15:28:50 +0800

    修改路径错误

--- Package.swift
+++ Package.swift
@@ -23,9 +23,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Smile",
-            dependencies: []),
+            dependencies: [],
+            path: "./Sources/"),
         .testTarget(
             name: "SmileTests",
-            dependencies: ["Smile"]),
+            dependencies: ["Smile"],
+            path: "./SmileTests/"),
     ]
 )
